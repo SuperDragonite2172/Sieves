2e9dc828cd1b48dbfc4d03917302cf6f3ad4ed2f
SuperDragonite2172 <superdragonite2172@gmail.com>
2021-07-24 23:28:41 -0400

    Algorithm Overhaul

--- SievePyV1.py
+++ SievePyV1.py
@@ -1,12 +1,13 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Prime Sieves - Python Edition                                 #
-# Written by SuperDragonite2172                                 #
-# Design Concept from Dave Plummer's Software Dragracing series #
-# Algorithm Used: Sieve of Eratosthenes                         #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Python Prime Sieve - Version 1                      #
+# Written by SuperDragonite2172                       #
+# Idea from Dave Plummer's Software Dragracing series #
+# Algorithm: Sieve of Eratosthenes (Implemented Well) #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 from argparse import ArgumentParser # For command-line arguments
 from timeit import default_timer # For timing the sieve
+from math import sqrt # Used by the sieve
 
 class PrimeSieve:
 
@@ -15,34 +16,31 @@ class PrimeSieve:
 
     def __init__(self, limit):
         self.size = limit
-        self.primes = [True] * self.size
+        self.primes = [True] * (self.size + 1)
     
     # runSieve: Runs the prime calculations.
     def runSieve(self):
-        factor = 3
-        self.primes[0] = False
+        for i in range(2, int(sqrt(len(self.primes)))+1):
+            if self.primes[i] == True:
+                for j in range(i*i, len(self.primes), i):
+                    self.primes[j] = False
+        self.primes = self.primes[1:]
+        self.primes[0] = None
         
-        while (factor < len(self.primes)):
-            for i in range(len(self.primes)):
-                if (i + 1 == factor or i + 1 == 2):
-                    pass
-                elif ((i + 1) % 2 == 0):
-                    self.primes[i] = False
-                else:
-                    if ((i + 1) % factor == 0):
-                        self.primes[i] = False
-            factor += 2
-    
     # printResults: Prints the statistics from the sieve run.
     def printResults(self, duration, passes, show_results):
         # TODO: Refine statistics.
         print("Time: %s\nPasses: %s\nAverage: %s" % (duration, passes, duration / passes))
+        if show_results:
+            print(dict(enumerate(self.primes,1)))
 
+# Command-line Parsing
 parser = ArgumentParser(description="Language: Python, Algorithm: Sieve of Eratosthenes, Dependencies: None")
 parser.add_argument("--limit"  , "-l", help="The upper limit of the sieve.", type=int, default=1000000)
 parser.add_argument("--time"   , "-t", help="How long the sieve will run.", type=float, default=10)
 parser.add_argument("--results", "-r", help="Display the primes found.", action="store_true")
 
+# Main Body
 args = parser.parse_args()
 limit = args.limit
 time_limit = args.time
